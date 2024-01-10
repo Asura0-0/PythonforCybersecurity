c33655b6dfeb3e1db8c18ccc3bbac33d17d9fa20
Asura50 <aahanbagchi@outlook.com>
2024-01-10 04:00:24 -0800

    HelloWorld3

--- start/CH02/HelloWorld3.py
+++ start/CH02/HelloWorld3.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
-# A more complex "Hello World" script in python with Inputs
-# Created 
+# A simple "Hello World" script in python with Inputs
+# Created Aahan - 1/10/24
+
+# Suggestion, build out 1 line at a time
+# Once multiple print statemetns exist, put a breakpoint at first print line
+# Then walk through as an example of "debugging"
+
+# Ask user for name
+user_name = input("What is your name? ")
+
+#print hello and their name
+print("Hello {0}".format(user_name))
+print(f"Hello {user_name}")
+print("Hello " + user_name)
+print("Hello", user_name)
+message = "hello " + user_name
+print(message)
