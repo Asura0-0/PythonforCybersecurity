36cc7ac6a84a94230eb1ef4167608bddacad2616
Asura50 <aahanbagchi@outlook.com>
2024-01-18 12:58:20 -0800

    Ping 4 functions

--- start/CH03/Funcy.py
+++ start/CH03/Funcy.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 # example workign with Functions
-#By 
+#By Aahan 1/18/24
 
 #This is my function
 def print_me( my_message):
     print(my_message)
+    return "it worked"
+
+def say_hello(num_times):
+    for x in range(num_times):
+        print("Hello World")
 
 #Calling the functio
 print_me("This is a function")
+result = print_me("this is another function with a return value")
+print(result)
+
+say_hello(3)
--- start/CH03/pinger4.py
+++ start/CH03/pinger4.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python3
-# Fourth example of pinging from Python
-# By 
+# Third example of pinging from Python
+# By Aahan 1/18/24
+
+import os
+import platform
+
+def ping_address(ip_address):
+    #find our os
+    current_os = platform.system().lower()
+    if current_os == "windows":
+        #build our ping cmd Windows
+        ping_cmd = "ping -n 1 -w 1000 {0} > NUL 2>&1".format(ip_address)
+    else:
+        #build our ping cmd Linux
+        ping_cmd = "ping -n 1 -w 1 {0} > /dev/null 2>&1".format(ip_address)
+    
+    # execute the ping cmd
+    exit_code = os.system(ping_cmd)
+    if exit_code == 0:
+        return True
+    return False
+
+#Assign IP address to a variable
+ip_prefix = "192.168.1."
+#for loop 1-255
+for octet in range (255):
+    # setup IP address
+    ip_address = ip_prefix + str(octet + 1)
+    
+    #call function
+    result = ping_address(ip_address)
+
+    # print results
+    if result == True:
+        print("{0} is online".format(ip_address))
