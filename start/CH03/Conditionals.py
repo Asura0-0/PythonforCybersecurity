5ae8a778a06324c0b4a7a83e946643cbfd93a777
Asura50 <aahanbagchi@outlook.com>
2024-01-21 13:56:29 -0800

    Functions

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -2,12 +2,16 @@
 # example workign with conditionals
 #By Aahan 1/14/24
 
+def send_message():
+    for i in range(10):
+        print("Yes it is")
+
 #Ask how day is
 day = input("Is today a good day? \n (y/n) ")
 
 #If statment for y
 if day == "y":
-    for i in range(10):
-        print("Yes it is")
+    send_message()
 else:
     print("Womp womp")
+
---
+++ start/CH03/deleteme.py
@@ -0,0 +1,14 @@
+
+
+#ask for input
+input = input("Enter a number ")
+
+#convert to int
+num = int(input)
+
+#check if even
+if num % 2 == 0:
+    print("{0} is an even number".format(num))
+else:
+    print("{0} is an odd number".format(num))
+
