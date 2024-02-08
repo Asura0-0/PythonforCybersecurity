a386999735b508126e32dddcc211f5d67fdcb9f3
Aahan <aahanbagchi@gmail.com>
2024-02-08 11:54:05 -0800

    more pain

--- start/CH06/FindThePAsswords.py
+++ start/CH06/FindThePAsswords.py
@@ -6,41 +6,31 @@ import crypt
 import os
 
 # Functions to test password
-def test_password(alorithm_salt, hashed_password, password_guess):
+def test_password(algorithm_salt, hashed_password, password_guess):
     # Use salt to has guess
-    hashed_guess = crypt.crypt(password_guess, alorithm_salt)
+    hashed_guess = crypt.crypt(password_guess, algorithm_salt)
     #compare salt guess against hashed password
     if hashed_guess == hashed_password:
         return True
     return False
 
-# Load dictionary file
-dir_path = os.path.dirname(os.path.realpath(__file__))
-f = open(dir_path+ "/shadow.txt", "r")
-pw = f.readlines()
-passwords = pw[11:]
-f.close
+# Get list of passwords to use
+with open("/home/aahan/PythonforCybersecurity/start/CH06/top10million.txt", "r") as f:
+    passwords = f.readlines()
 
-# Promt user for Algorithm/salt
-algorithm_salt = input("What is the algorithm and salt? ")
+# Get the hashed password to crack
+with open("/home/aahan/PythonforCybersecurity/start/CH06/shadow", "r") as f:
+    pw = f.readlines()
+    hashed = pw[11:]
 
-# Promt user for salt has
-hashed_password = input("what is the full hashed password? ")
-
-# Loop through each password
-for password in passwords:
+for password, hash in zip(passwords, hashed):
     password = password.strip()
+    hash = hash.strip()
+    algorithm_salt = hash[12:27]
+    hashed_password = hash[29:115]
+        
     result = test_password(algorithm_salt, hashed_password, password)
     if result:
         print("MAtch found: {0}".format(password))
         break
-
-for password in passwords:
-    password = password.strip()
-    result = test_password(algorithm_salt, hashed_password, password)
-    if result:
-        print("MAtch found: {0}".format(password))
-        break
-
-
 
