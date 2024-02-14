b07fbafbe68b0d3bc4d9181d553dc41731796294
Asura50 <aahanbagchi@outlook.com>
2024-02-13 23:11:09 -0800

    Week 7

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for 404 errors
-# By 
+# By Aahan 2/13/24
+
+import os
+# prompt for file
+log_file = input("which file do you want to scan? ")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+f = open(dir_path+ "/"+ log_file, "r")
+
+# Read file line by line
+while True:
+    line = f.readline()
+    if not line:
+        break
+    # Loop to look for 404
+    # if "404" in line:
+    #     print(line.strip())
+    if "162.239.205.0" in line:
+        if "404" in line:
+            print(line.strip())
+
+# Close file
+f.close()
--- start/CH07/FindStatusCodes.py
+++ start/CH07/FindStatusCodes.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 # Script that scans web server logs for status codes
 # Use RegEx to find and report on most frequent status messages
-# By 
+# By #!/usr/bin/env python3
+# Script that scans web server logs for 404 errors
+# By Aahan 2/13/24
+
+import os
+import re
+# prompt for file
+log_file = input("which file do you want to scan? ")
+dir_path = os.path.dirname(os.path.realpath(__file__))
+f = open(dir_path+ "/"+ log_file, "r")
+
+# setup regex pattern and dictionary
+
+
+# Find match in file an dstore in dictionary
+
+        # Search for pattern, i found, store it
+
+
+
+# sort and print most frequent result
